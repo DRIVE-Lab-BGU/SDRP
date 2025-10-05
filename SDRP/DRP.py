6f0219b7d53a6e4b8dbea32aa84df707139b8685
yuvalaro <yuvalaroosh9@gmail.com>
2025-10-05 11:27:54 +0000

    runing for instance 3 sd 1

--- SDRP/DRP.py
+++ SDRP/DRP.py
@@ -27,7 +27,7 @@ from planner import (
 ###############################################
 
 base_path = os.path.dirname(os.path.abspath(__file__))
-instance = "instance_2.rddl"
+instance = "instance_3.rddl"
 problem = "reservoir"
 domain_file = os.path.join(base_path, "instances", problem, "domain.rddl")
 instance_file = os.path.join(base_path, "instances", problem, instance)
