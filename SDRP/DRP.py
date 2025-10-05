ce453acec47d9673b68e401a5ebedc1db66fab66
yuvalaro <yuvalaroosh9@gmail.com>
2025-10-05 11:26:57 +0000

    runing for instance 3 sd 1

--- SDRP/DRP.py
+++ SDRP/DRP.py
@@ -63,7 +63,7 @@ basename = instance
 
 
 
-csv_file = os.path.join(base_path, f"log_{instance[0:-5]}_sd = 1.csv")
+csv_file = os.path.join(base_path, f"log_{instance[0:-5]}_sd = 5.csv")
 write_header = not os.path.exists(csv_file)
 
 # Open the file in append mode
