2eaf3d21b82bb73451e5d837dca299bb7ccf6e13
yuvalaro <yuvalaroosh9@gmail.com>
2025-10-12 07:19:18 +0000

    runing

--- SDRP/DRP.py
+++ SDRP/DRP.py
@@ -34,7 +34,7 @@ base_path = os.path.dirname(os.path.abspath(__file__))
 ##################
 instance = "instance_3.rddl"
 problem = "reservior"
-sd_for_stoc = 3
+sd_for_stoc = 5
 pass_for_save = f"log_{instance[0:-5]}_sd = {sd_for_stoc}.csv"
 
 
