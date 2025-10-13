b3613352fcfb54d805443c14e605157cfc3a49a9
ataitler <ataitler@gmail.com>
2025-10-13 14:35:44 +0300

    noise print

--- SDRP/core/policies.py
+++ SDRP/core/policies.py
@@ -324,7 +324,7 @@ class StochasticJaxPolicy(DeterministicJaxPolicy):
         actions = self.planner.get_action(
             subkey, self.params, self.step, state, self.eval_hyperparams)
         self.step += 1
-        print(self.exploration_noise)
+        print("testing noise in sample action:", self.exploration_noise)
         sys.exit(0)
         for key in actions:
             actions[key] = actions[key] + np.random.normal(loc=0, scale=self.exploration_noise, size=actions[key].shape)
