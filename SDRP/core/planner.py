a26e008d60c163c68e3908a75eb186379909e1ae
ataitler <ataitler@gmail.com>
2025-11-24 14:57:25 +0200

    debug print for IS

--- SDRP/core/planner.py
+++ SDRP/core/planner.py
@@ -2494,6 +2494,7 @@ class JaxBackpropPlanner:
         def _jax_wrapped_plan_loss(key, policy_params, policy_hyperparams, subs, model_params):
             # Roll out with the (possibly noisy) behavior policy
             log, model_params = rollouts(key, policy_params, policy_hyperparams, subs, model_params)
+            print(log["action"])
             log = _ensure_action_dict_in_log(log)
 
             rewards = log['reward']  # [B, T]
