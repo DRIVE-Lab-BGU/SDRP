ffd2d8b86fe83a615e0ec2c5885cc5c76172d3db
yuvalaroosh <yuvalaroosh9@gmail.com>
2026-02-18 15:07:02 +0200

    try to fix sampler reward

--- SDRP/core_torch/simulator.py
+++ SDRP/core_torch/simulator.py
@@ -4,6 +4,14 @@ from pyRDDLGym.core.simulator import RDDLSimulator
 import torch
 import numpy as np
 from pyRDDLGym.core.simulator import RDDLSimulator
+from pyRDDLGym.core.env import RDDLEnv
+from pyRDDLGym.core.compiler.model import RDDLLiftedModel
+import pyRDDLGym
+import os
+from pyRDDLGym.core.simulator import RDDLSimulator
+from pyRDDLGym.core.env import RDDLEnv
+from pyRDDLGym.core.compiler.model import RDDLLiftedModel
+
 # TODO:
 # 1. init 
 # 2. step
@@ -38,14 +46,25 @@ class Simulator(RDDLSimulator):
             return action_observation.cpu().numpy()
 
 
-    def show(self):
-        # Display the current state of the environment
-        if self.state is not None:
-            print("Current state:", self.state)
+    def sample_action(self):
+        """Sample an action from the environment's action space."""
+        action = self.env.action_space.sample()
+        return action
+    
+    def sample_reward(self):
+        """Sample a reward from the environment's reward space."""
+        reward = self.env.sample_reward()
+        if isinstance(reward, dict):
+            return {k: torch.tensor(v, dtype=torch.float32, device=self.device) for k, v in reward.items()}
         else:
-            print("State is not initialized.")
+            return torch.tensor(reward, dtype=torch.float32, device=self.device)    
 
 
+    def sample_observation(self):
+        """Sample an observation from the environment's observation space."""
+        observation = self.env.observation_space.sample()
+        return observation
+    
     def step(self, action):
         
         pass
@@ -66,10 +85,75 @@ class Simulator(RDDLSimulator):
         # Set the random seed for reproducibility
         pass
 def main():
+    ##
+    ##############################################################################################
+    #     Create the enviroxnment with config      #
+    ##############################################################################################
+    base_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    #print(f'-------------------base_path: {base_path}')
+    domain_rddl   = os.path.join(base_path, "instances", "reservoir", "domain.rddl")
+    instance_rddl = os.path.join(base_path, "instances", "reservoir", "instance_1.rddl")
 
+    myEnv = pyRDDLGym.make(
+        domain=domain_rddl,
+        instance=instance_rddl,
+        vectorized=True
+    )
+    ##############################################################################################
+    
+    
+    # here i have a problem ' if i want ro gradient the action (foe example in race car) 
+    # i need to take the tensor for each action but in some cases the action is a dict
+    # for some action ( e.g. in reservation tere is only one action - 'release')
+    # not only for gar also to convert
+    
+    print("################################## Actions ##########################################")
+    
     # This is just a demo of conversion utilities (does not step an env).
-    sim = Simulator(env=None)
+    sim = Simulator(env=myEnv)
+    an_action = sim.sample_action()
+    #print(f'Sample action from the environment: {an_action}')
+    #print ('try to coonver an actionto torch:', sim.array2torch(an_action))
+    print(f'Action as numpy array: {an_action}')
+    # Convert the action to a torch tensor  
+    x = sim.array2torch(an_action)
+    print(f'Action as torch tensor: {x}')
+    # return x to a numpy array
+    x_np = sim.torch2array(x)
+    print(f'Action as numpy array from torch: {x_np}')  
 
+    print("################################## Observations ##########################################")
+    an_ovservation = sim.sample_observation()
+    print(f'Sample observation from the environment: {an_ovservation}')
+    # Convert the observation to a torch tensor
+    y = sim.array2torch(an_ovservation)
+    print(f'Observation as torch tensor: {y}')
+    # return y to a numpy array
+    y_np = sim.torch2array(y)
+    print(f'Observation as numpy array from torch: {y_np}')
+    
+
+
+
+    print("################################## reward ##########################################")
+    an_reward = sim.sample_reward()
+    print(f'Sample reward from the environment: {an_reward}')
+
+
+
+    print(f'Sample reward from the environment: {an_reward}')
+    # Convert the reward to a torch tensor
+    z = sim.array2torch(an_reward)
+    print(f'Reward as torch tensor: {z}')
+    # return z to a numpy array
+    z_np = sim.torch2array(z)
+    print(f'Reward as numpy array from torch: {z_np}')
+
+
+
+    exit()
+    print(f'sample an action from the environment: {sim.sample_action()}')
+   
     obs_np = {'release': np.array([17.89675 , 15.712295], dtype=np.float32)}
     obs_t = sim.array2torch(obs_np)
     print("Converted to torch:", obs_t)
